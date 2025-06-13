2bb9d50d00ec8df53e2904dc3bae5bb0f445dc36
Lou Morda <lmorda2@hotmail.com>
2025-06-13 16:39:08 -0700

    Revert accidental view model test rename

--- app/src/test/java/com/lmorda/homework/ExploreViewModelTest.kt
+++ app/src/test/java/com/lmorda/homework/ExploreViewModelTest.kt
@@ -25,7 +25,7 @@ import org.junit.Rule
 import org.junit.Test
 
 @OptIn(ExperimentalCoroutinesApi::class)
-class ContactsViewModelTest {
+class ExploreViewModelTest {
 
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
